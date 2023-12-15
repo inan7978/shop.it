c994da881841e11673cae910d77fe7ccc4b3bcfc
Ismailov <Iismailov@hospiceatcharlotte.net>
2023-12-15 14:00:57 -0500

    created the log in page as well

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,24 @@
 function LoginPage() {
   return (
     <>
-      <div>This is the Login Page</div>
+      <>
+        <div className="login-page" style={{ background: "#7eb2dd" }}>
+          <div className="form-container">
+            <h3 className="heading-bold">Log In!</h3>
+            <form className="login-form">
+              <label className="field-label">
+                Email:
+                <input className="input-single" type="email" />
+              </label>
+              <label className="field-label">
+                Password:
+                <input className="input-single" type="password" />
+              </label>
+              <input className="login-submit" type="submit" />
+            </form>
+          </div>
+        </div>
+      </>
     </>
   );
 }
