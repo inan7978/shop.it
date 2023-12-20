636d9ca5bf0919213582fdd23534d2de5cc6f25b
Ismailov <Iismailov@hospiceatcharlotte.net>
2023-12-20 17:13:48 -0500

    added more structure to the mycartpage

--- src/pages/MyCartPage.js
+++ src/pages/MyCartPage.js
@@ -1,6 +1,6 @@
 import UserContext from "../context/UserContext";
 import CartContext from "../context/CartContext";
-import ItemCard from "../components/ItemCard"
+import ItemCard from "../components/ItemCard";
 import { useContext } from "react";
 function MyCartPage() {
   const { loadCart } = useContext(CartContext);
@@ -9,10 +9,26 @@ function MyCartPage() {
 
   const cartItems = cart.map((item) => {
     // return <h2 key={item.item}>{item.itemName}</h2>;
-    return <ItemCard key={item.item} item={item} />
+    return <ItemCard key={item.item} item={item} />;
   });
   return (
-    <>{cart.length ? <div className="item-cards-container">{cartItems}</div> : <div>Your cart is empty</div>}</>
+    <>
+      {cart.length ? (
+        <div className="cart-page-container">
+          <div className="item-cards-container">{cartItems}</div>
+          <div className="total-costs">
+            <h3>Sub Total:</h3>
+            <h3>Tax:</h3>
+            <h3>Rewards:</h3>
+            <h3>Fees:</h3>
+            <h3>Shipping:</h3>
+            <h2>Grand Total:</h2>
+          </div>
+        </div>
+      ) : (
+        <div>Your cart is empty</div>
+      )}
+    </>
   );
 }
 
