e1ace786d5b8f08669ac2af6998531a3cc8a7d52
Inan Ismailov <inan7978@icloud.com>
2023-12-20 00:27:10 -0500

    added style to the shopping cart items, still needs more

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,10 +1,15 @@
+import stock_photo from "../images/boxes_stock.png";
+
 function ItemCard(props) {
-
-
-  return <article className="item-card">
-    <h1>{props.item.itemName} | ${props.item.Price}</h1>
-    <p>{props.item.description}</p>
-  </article>;
+  return (
+    <article className="item-card">
+      <img src={stock_photo} className="item-card-img" />
+      <h1 className="card-title">
+        {props.item.itemName} | ${props.item.Price}
+      </h1>
+      <p className="card-description">{props.item.description}</p>
+    </article>
+  );
 }
 
 export default ItemCard;
--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -7,14 +7,54 @@ const CartContext = createContext();
 export function CartProvider({ children }) {
   const { user } = useContext(UserContext);
   const [cart, setCart] = useState([
-    { item: 3324435, itemName: "Toothbrush", Price: 1, description: "Handy for nice teeth." },
-    { item: 3324415, itemName: "Shirt", Price: 10, description: "Handy if you want to be served at restaurants." },
-    { item: 3624435, itemName: "Socks", Price: 5, description: "Keep your dogs warm." },
-    { item: 7324435, itemName: "Apple", Price: 1.5, description: "One of these a day keeps the doctor away." },
-    { item: 7324435, itemName: "Onion", Price: 1.5, description: "One of these keeps EVERYONE away." },
-    { item: 7324435, itemName: "Chocolate", Price: 1.5, description: "A tasty dessert." },
-    { item: 7324435, itemName: "Water", Price: 1.5, description: "You are 60% of it. Make sure you drink enough." },
-    { item: 7324435, itemName: "Cigarettes", Price: 1.5, description: "Everyone needs one every now and then." },
+    {
+      item: 10,
+      itemName: "Toothbrush",
+      Price: 1,
+      description: "Handy for nice teeth.",
+    },
+    {
+      item: 11,
+      itemName: "Shirt",
+      Price: 10,
+      description: "Handy if you want to be served at restaurants.",
+    },
+    {
+      item: 12,
+      itemName: "Socks",
+      Price: 5,
+      description: "Keep your dogs warm.",
+    },
+    {
+      item: 13,
+      itemName: "Apple",
+      Price: 1.5,
+      description: "One of these a day keeps the doctor away.",
+    },
+    {
+      item: 14,
+      itemName: "Onion",
+      Price: 1.5,
+      description: "One of these keeps EVERYONE away.",
+    },
+    {
+      item: 15,
+      itemName: "Chocolate",
+      Price: 1.5,
+      description: "A tasty dessert.",
+    },
+    {
+      item: 16,
+      itemName: "Water",
+      Price: 1.5,
+      description: "You are 60% of it. Make sure you drink enough.",
+    },
+    {
+      item: 17,
+      itemName: "Cigarettes",
+      Price: 1.5,
+      description: "Everyone needs one every now and then.",
+    },
   ]);
 
   function loadCart() {
