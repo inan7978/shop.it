5d270c09dc550cbac513f77d77fda026eb469244
Ismailov <Iismailov@hospiceatcharlotte.net>
2023-12-21 22:32:59 -0500

    worked on it

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -2,12 +2,16 @@ import stock_photo from "../images/boxes_stock.png";
 
 function ItemCard(props) {
   return (
-    <article className="item-card">
+    <article
+      className="item-card"
+      onClick={() => {
+        console.log(`${props.item.itemName} has been clicked.`);
+      }}
+    >
       <img src={stock_photo} className="item-card-img" />
-      <h1 className="card-title">
-        {props.item.itemName} | ${props.item.Price}
-      </h1>
-      <p className="card-description">{props.item.description}</p>
+      <h1 className="card-title">{props.item.itemName}</h1>
+      <h3>${props.item.Price}</h3>
+      <button>Remove</button>
     </article>
   );
 }
