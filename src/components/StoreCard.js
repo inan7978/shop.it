d024f572e4a3ddc44d5b660294756ca6d56fdb8f
Ismailov <inan7978@icloud.com>
2024-01-18 09:24:38 -0500

    home page now directs you to cart when you click to add to cart if you are not logged in

--- src/components/StoreCard.js
+++ src/components/StoreCard.js
@@ -12,6 +12,7 @@ function StoreCard(props) {
   const navigate = useNavigate();
 
   const { addToCart } = useContext(UserContext);
+  const { loggedIn } = useContext(UserContext);
 
   return (
     <article
@@ -41,16 +42,28 @@ function StoreCard(props) {
       />
       <h1 className="store-card-title">{props.item.title}</h1>
       <h3>{modPrice}</h3>
-      <button
-        className="add-cart-btn btn-submit"
-        onClick={(e) => {
-          e.stopPropagation(); // this is used because this onClick is located within a broader on click. Not including this would have both on clicks fire when this is selected.
-          addToCart(props.item._id);
-          console.log(props.item.title + " request to add to cart.");
-        }}
-      >
-        Add to Cart
-      </button>
+      {loggedIn ? (
+        <button
+          className="add-cart-btn btn-submit"
+          onClick={(e) => {
+            e.stopPropagation(); // this is used because this onClick is located within a broader on click. Not including this would have both on clicks fire when this is selected.
+            addToCart(props.item._id);
+            console.log(props.item.title + " request to add to cart.");
+          }}
+        >
+          Add to Cart
+        </button>
+      ) : (
+        <button
+          className="btn-submit"
+          onClick={(e) => {
+            e.stopPropagation();
+            navigate("/login");
+          }}
+        >
+          Add to Cart
+        </button>
+      )}
     </article>
   );
 }
