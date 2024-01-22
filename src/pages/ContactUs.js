e495fbf478a02095a43d6c920e027052a8148d2b
Ismailov <inan7978@icloud.com>
2024-01-22 16:52:47 -0500

    started the contact us page

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,8 +1,30 @@
 function ContactUs() {
   return (
-    <>
-      <div>This is the Contact Us Page</div>
-    </>
+    <div className="contact-us-container">
+      <h2 className="contact-us-heading">Contact Us</h2>
+      <div className="img-desc-container">
+        <img className="contact-us-img" src={"https://picsum.photos/500"} />
+        <div className="contact-us-text-container">
+          <p className="contact-us-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec
+            feugiat in fermentum posuere urna nec. Elementum curabitur vitae
+            nunc sed. Facilisi nullam vehicula ipsum a arcu cursus vitae congue
+            mauris. Velit dignissim sodales ut eu sem integer vitae. Ac turpis
+            egestas maecenas pharetra convallis. Adipiscing elit ut aliquam
+            purus sit amet luctus. Tellus orci ac auctor augue mauris augue.
+            Pulvinar sapien et ligula ullamcorper malesuada proin libero. Massa
+            sed elementum tempus egestas sed. Scelerisque viverra mauris in
+            aliquam sem fringilla. Etiam dignissim diam quis enim lobortis
+            scelerisque fermentum. Gravida in fermentum et sollicitudin ac.
+            Phasellus faucibus scelerisque eleifend donec pretium vulputate
+            sapien nec. Ultrices tincidunt arcu non sodales neque sodales ut.
+            Posuere sollicitudin aliquam ultrices sagittis. At varius vel
+            pharetra vel turpis nunc eget lorem. Lectus quam id leo in vitae.
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
 
