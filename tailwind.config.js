34f8e8fdab432ff35b71e92bac9eedc9ef0607ef
Inan Ismailov <inan7978@icloud.com>
2024-02-09 18:20:42 -0600

    redid the header with Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        theBlue: "#7eb2dd",
+      },
+    },
   },
   plugins: [],
 };
