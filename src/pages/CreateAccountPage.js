fa249dfef2e6020bec46b6bc981c6cd16390fd48
Inan Ismailov <inan7978@icloud.com>
2024-02-13 22:53:22 -0500

    fixed the ID of the confirm password input field not being set properly in the signup page

--- src/pages/CreateAccountPage.js
+++ src/pages/CreateAccountPage.js
@@ -117,8 +117,8 @@ function CreateAccountPage() {
           />
           <input
             type="password"
-            id="password"
-            name="password"
+            id="confirm-password"
+            name="confirm-password"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg mx-auto my-4 h-12 bg-gray-300"
             placeholder="Re-Enter Password"
             value={confirmPass}
