b4b54be343a19f4afea64fcd6a9dbfaa0ece6c97
Ismailov <inan7978@icloud.com>
2024-02-20 14:57:08 -0500

    search results can now be clicked and will take you to the page

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function SearchBox() {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const toFind = { search: searchTerm };
@@ -45,13 +48,25 @@ function SearchBox() {
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg mx-auto my-10 h-12 bg-gray-300"
         />
       </div>
-
       <div className="absolute mt-24">
         <ul className="bg-theBlue w-96 rounded">
           {searchResults
             ? searchResults.map((result) => (
-                <li key={result._id}>
-                  <div className="p-4 mb-5 flex gap-3 items-center hover:bg-red-500 rounded">
+                <li
+                  key={result._id}
+                  onClick={() => {
+                    navigate(`../item-details-page/${result._id}`, {
+                      state: {
+                        item: result.title,
+                        description: result.description,
+                        price: JSON.parse(result.price.$numberDecimal),
+                        imgURL: result.imgURL,
+                        id: result._id,
+                      },
+                    });
+                  }}
+                >
+                  <div className="p-4 mb-5 last:mb-0 flex gap-3 items-center hover:bg-red-500 rounded">
                     <img className="w-10 h-10" src={result.imgURL[0]} />
                     <h1 className="text-white font-bold">{result.title}</h1>
                   </div>
