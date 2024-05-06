fe747da5bbdad67da283ffc575b0c4ed156a3123
Ismailov <inan7978@icloud.com>
2024-05-06 17:06:10 -0400

    the shopping cart page has been redone and needs to have a few more things added to it

--- src/api/myCartPageAPI.js
+++ src/api/myCartPageAPI.js
@@ -1,12 +1,12 @@
 import { BASE_URL } from "./baseURL";
 
-export async function _loadDetails(toFind) {
+export async function _loadDetails(user) {
   const result = await fetch(`${BASE_URL}/get-cart-items`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(toFind),
+    body: JSON.stringify({ user: user }),
   });
 
   const data = await result.json();
