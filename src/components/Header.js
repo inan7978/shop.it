dac2d478bfbbe26f6a60ecbf2f32690426f50b46
Ismailov <Iismailov@hospiceatcharlotte.net>
2024-05-13 15:29:18 -0400

    re-did the header. It is now a component that renders a different header based on whether someone is logged in or not. Looks much cleaner

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import shopItlogo from "../images/shopitLogo.png";
 import UserContext from "../context/UserContext";
 import Hamburger from "./Hamburger";
+import LoggedInHeader from "./LoggedInHeader";
+import LoggedOutHeader from "./LoggedOutHeader";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { useContext } from "react";
@@ -20,191 +22,197 @@ function Header() {
     setMenuOpen(!menuOpen);
   };
 
-  return (
-    <div className="flex justify-between w-1/1 bg-theBlue">
-      {/* This is the hamburger menu */}
-      {menuOpen &&
-        // hamburger menu when someone is logged in
-        (Object.keys(user).length ? (
-          <div className="container absolute w-screen">
-            <>
-              <div className="bg-theBlue w-full h-screen mx-auto flex flex-col py-24">
-                <button
-                  className={menuButtons}
-                  onClick={() => {
-                    navigate("./my-cart");
-                    setMenuOpen(false);
-                  }}
-                >{`Cart: ${user.cart.length}`}</button>
-                <button
-                  onClick={() => {
-                    navigate("./my-account");
-                    setMenuOpen(false);
-                  }}
-                  className={menuButtons}
-                >
-                  Account
-                </button>
-                <button
-                  className={menuButtons}
-                  onClick={() => {
-                    navigate("./list-item");
-                    setMenuOpen(false);
-                  }}
-                >
-                  Sell something!
-                </button>
-                <button
-                  className={menuButtons}
-                  onClick={() => {
-                    navigate("./contact-us");
-                    setMenuOpen(false);
-                  }}
-                >
-                  Contact Us
-                </button>
-                <button
-                  className={menuButtons}
-                  onClick={() => {
-                    navigate("./privacy");
-                    setMenuOpen(false);
-                  }}
-                >
-                  Privacy Policy
-                </button>
-                <button
-                  className={menuButtons}
-                  onClick={() => {
-                    navigate("./about");
-                    setMenuOpen(false);
-                  }}
-                >
-                  About
-                </button>
-                <button
-                  className={menuButtons}
-                  onClick={() => {
-                    logOutUser("toStore");
-                    setMenuOpen(false);
-                  }}
-                >
-                  Log out!
-                </button>
-              </div>
-            </>
-          </div>
-        ) : (
-          // hamburger menu when no one is logged in
-          <div className="container absolute w-screen">
-            <>
-              <div className="bg-theBlue w-full h-screen mx-auto flex flex-col py-24">
-                <button
-                  onClick={() => {
-                    navigate("./login");
-                    setMenuOpen(false);
-                  }}
-                  className={menuButtons}
-                >
-                  Log In
-                </button>
-                <button
-                  onClick={() => {
-                    navigate("./sign-up");
-                    setMenuOpen(false);
-                  }}
-                  className={menuButtons}
-                >
-                  Sign up!
-                </button>
-                <button
-                  className={menuButtons}
-                  onClick={() => {
-                    navigate("./contact-us");
-                    setMenuOpen(false);
-                  }}
-                >
-                  Contact Us
-                </button>
-                <button
-                  className={menuButtons}
-                  onClick={() => {
-                    navigate("./privacy");
-                    setMenuOpen(false);
-                  }}
-                >
-                  Privacy Policy
-                </button>
-                <button
-                  className={menuButtons}
-                  onClick={() => {
-                    navigate("./about");
-                    setMenuOpen(false);
-                  }}
-                >
-                  About
-                </button>
-              </div>
-            </>
-          </div>
-        ))}
-      <div className="flex">
-        <img
-          className="w-1/3 sm:w-1/4 cursor-pointer"
-          onClick={() => navigate("./store-page")}
-          src={shopItlogo}
-          alt="shop.it logo"
-        />
-      </div>
-      <div className="hidden md:flex justify-end gap-5 items-center w-1/4 p-2">
-        {Object.keys(user).length ? (
-          <>
-            <button
-              onClick={() => {
-                navigate("./my-account");
-              }}
-              className={buttonsStyles}
-            >
-              Account
-            </button>
-            <button
-              className={buttonsStyles}
-              onClick={() => logOutUser("toStore")}
-            >
-              Log out!
-            </button>
-            <button
-              className={buttonsStyles}
-              onClick={() => navigate("./my-cart")}
-            >{`Cart`}</button>
-          </>
-        ) : (
-          <>
-            <button
-              onClick={() => {
-                navigate("./login");
-              }}
-              className={buttonsStyles}
-            >
-              Log In
-            </button>
-            <button
-              onClick={() => {
-                navigate("./sign-up");
-              }}
-              className={buttonsStyles}
-            >
-              Sign up!
-            </button>
-          </>
-        )}
-      </div>
-      <div
-        className="flex justify-center items-center mr-3 md:hidden"
-        onClick={toggleMenu}
-      >
-        <Hamburger />
-      </div>
-    </div>
+  // return (
+  //   <div className="flex justify-between w-1/1 bg-theBlue">
+  //     {/* This is the hamburger menu */}
+  //     {menuOpen &&
+  //       // hamburger menu when someone is logged in
+  //       (Object.keys(user).length ? (
+  //         <div className="container absolute w-screen">
+  //           <>
+  //             <div className="bg-theBlue w-full h-screen mx-auto flex flex-col py-24">
+  //               <button
+  //                 className={menuButtons}
+  //                 onClick={() => {
+  //                   navigate("./my-cart");
+  //                   setMenuOpen(false);
+  //                 }}
+  //               >{`Cart: ${user.cart.length}`}</button>
+  //               <button
+  //                 onClick={() => {
+  //                   navigate("./my-account");
+  //                   setMenuOpen(false);
+  //                 }}
+  //                 className={menuButtons}
+  //               >
+  //                 Account
+  //               </button>
+  //               <button
+  //                 className={menuButtons}
+  //                 onClick={() => {
+  //                   navigate("./list-item");
+  //                   setMenuOpen(false);
+  //                 }}
+  //               >
+  //                 Sell something!
+  //               </button>
+  //               <button
+  //                 className={menuButtons}
+  //                 onClick={() => {
+  //                   navigate("./contact-us");
+  //                   setMenuOpen(false);
+  //                 }}
+  //               >
+  //                 Contact Us
+  //               </button>
+  //               <button
+  //                 className={menuButtons}
+  //                 onClick={() => {
+  //                   navigate("./privacy");
+  //                   setMenuOpen(false);
+  //                 }}
+  //               >
+  //                 Privacy Policy
+  //               </button>
+  //               <button
+  //                 className={menuButtons}
+  //                 onClick={() => {
+  //                   navigate("./about");
+  //                   setMenuOpen(false);
+  //                 }}
+  //               >
+  //                 About
+  //               </button>
+  //               <button
+  //                 className={menuButtons}
+  //                 onClick={() => {
+  //                   logOutUser("toStore");
+  //                   setMenuOpen(false);
+  //                 }}
+  //               >
+  //                 Log out!
+  //               </button>
+  //             </div>
+  //           </>
+  //         </div>
+  //       ) : (
+  //         // hamburger menu when no one is logged in
+  //         <div className="container absolute w-screen">
+  //           <>
+  //             <div className="bg-theBlue w-full h-screen mx-auto flex flex-col py-24">
+  //               <button
+  //                 onClick={() => {
+  //                   navigate("./login");
+  //                   setMenuOpen(false);
+  //                 }}
+  //                 className={menuButtons}
+  //               >
+  //                 Log In
+  //               </button>
+  //               <button
+  //                 onClick={() => {
+  //                   navigate("./sign-up");
+  //                   setMenuOpen(false);
+  //                 }}
+  //                 className={menuButtons}
+  //               >
+  //                 Sign up!
+  //               </button>
+  //               <button
+  //                 className={menuButtons}
+  //                 onClick={() => {
+  //                   navigate("./contact-us");
+  //                   setMenuOpen(false);
+  //                 }}
+  //               >
+  //                 Contact Us
+  //               </button>
+  //               <button
+  //                 className={menuButtons}
+  //                 onClick={() => {
+  //                   navigate("./privacy");
+  //                   setMenuOpen(false);
+  //                 }}
+  //               >
+  //                 Privacy Policy
+  //               </button>
+  //               <button
+  //                 className={menuButtons}
+  //                 onClick={() => {
+  //                   navigate("./about");
+  //                   setMenuOpen(false);
+  //                 }}
+  //               >
+  //                 About
+  //               </button>
+  //             </div>
+  //           </>
+  //         </div>
+  //       ))}
+  //     <div className="flex">
+  //       <img
+  //         className="w-1/3 sm:w-1/4 cursor-pointer"
+  //         onClick={() => navigate("./store-page")}
+  //         src={shopItlogo}
+  //         alt="shop.it logo"
+  //       />
+  //     </div>
+  //     <div className="hidden md:flex justify-end gap-5 items-center w-1/4 p-2">
+  //       {Object.keys(user).length ? (
+  //         <>
+  //           <button
+  //             onClick={() => {
+  //               navigate("./my-account");
+  //             }}
+  //             className={buttonsStyles}
+  //           >
+  //             Account
+  //           </button>
+  //           <button
+  //             className={buttonsStyles}
+  //             onClick={() => logOutUser("toStore")}
+  //           >
+  //             Log out!
+  //           </button>
+  //           <button
+  //             className={buttonsStyles}
+  //             onClick={() => navigate("./my-cart")}
+  //           >{`Cart`}</button>
+  //         </>
+  //       ) : (
+  //         <>
+  //           <button
+  //             onClick={() => {
+  //               navigate("./login");
+  //             }}
+  //             className={buttonsStyles}
+  //           >
+  //             Log In
+  //           </button>
+  //           <button
+  //             onClick={() => {
+  //               navigate("./sign-up");
+  //             }}
+  //             className={buttonsStyles}
+  //           >
+  //             Sign up!
+  //           </button>
+  //         </>
+  //       )}
+  //     </div>
+  //     <div
+  //       className="flex justify-center items-center mr-3 md:hidden"
+  //       onClick={toggleMenu}
+  //     >
+  //       <Hamburger />
+  //     </div>
+  //   </div>
+  // );
+
+  return user._id ? (
+    <LoggedInHeader logOutUser={logOutUser} />
+  ) : (
+    <LoggedOutHeader />
   );
 }
 
